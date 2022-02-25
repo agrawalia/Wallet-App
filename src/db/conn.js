0c7b704f91b0c99a881513d416c079d6dd9cec1f
Ishan Agrawal <agrawalia@rknec.edu>
2022-02-25 22:25:49 +0530

    MongoDB env variable
    MongoDB env variable

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,13 +1,11 @@
 const mongoose = require("mongoose");
+require("dotenv/config");
 mongoose
-  .connect(
-    "mongodb+srv://maxmilain:maxmilain@cluster0.c9tus.mongodb.net/wallet",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      // useCreateIndex: true
-    }
-  )
+  .connect(process.env.MONGODB_CONNECT, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useCreateIndex: true
+  })
   .then(() => {
     console.log("connection successfull");
   })
